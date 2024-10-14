6c7546863081149acfd38764631f106c59945403
j@50nl33 <jaisonyi@gmail.com>
2024-10-14 22:15:23 +0800

    Update script.sh

--- memory_privileges/script.sh
+++ memory_privileges/script.sh
@@ -1,6 +1,18 @@
-#!/bin/bash
-# Vulnerable shell script with memory limit issue
-export JAVA_OPTS="-Xmx2048m"
+# Vulnerable Terraform file with memory limit issue
+resource "null_resource" "example_memory_limit" {
+  provisioner "local-exec" {
+    command = <<-EOF
+      #!/bin/bash
+      export JAVA_OPTS="-Xmx2048m"
+    EOF
+  }
+}
 
 # Privileged execution
-sudo su -c "echo Hello"
+resource "null_resource" "example_privileged_execution" {
+  provisioner "local-exec" {
+    command = <<-EOF
+      sudo su -c "echo Hello"
+    EOF
+  }
+}
