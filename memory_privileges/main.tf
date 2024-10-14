7abc5045481bc6a90a7b2fc58d2508b286f0517e
j@50nl33 <jaisonyi@gmail.com>
2024-10-14 21:54:20 +0800

    Update main.tf

--- memory_privileges/main.tf
+++ memory_privileges/main.tf
@@ -2,26 +2,26 @@
 resource "aws_instance" "example" {
   instance_type = "t2.micro"
   user_data = <<-EOF
-              #!/bin/bash
-              export JAVA_OPTS="-Xmx2048m"
-              EOF
+    #!/bin/bash
+    export JAVA_OPTS="-Xmx2048m"
+  EOF
 }
 
 # Privileged execution
 resource "aws_iam_role" "example" {
   name = "example-role"
   assume_role_policy = <<-EOF
-                      {
-                        "Version": "2012-10-17",
-                        "Statement": [
-                          {
-                            "Effect": "Allow",
-                            "Principal": {
-                              "Service": "ec2.amazonaws.com"
-                            },
-                            "Action": "sts:AssumeRole"
-                          }
-                        ]
-                      }
-                      EOF
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Principal": {
+            "Service": "ec2.amazonaws.com"
+          },
+          "Action": "sts:AssumeRole"
+        }
+      ]
+    }
+  EOF
 }
