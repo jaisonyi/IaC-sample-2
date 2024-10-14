0d4a752a238876a7561cb899582e3b808be30629
j@50nl33 <jaisonyi@gmail.com>
2024-10-14 22:08:30 +0800

    Update script.bat

--- memory_privileges/script.bat
+++ memory_privileges/script.bat
@@ -1,5 +1,18 @@
-@echo off
-REM Vulnerable batch script with memory limit issue
-set JAVA_OPTS=-Xmx2048m
-REM Privileged execution
-runas /user:Administrator "cmd.exe /C echo Hello"
+# Vulnerable Terraform file with memory limit issue
+resource "null_resource" "example_memory_limit" {
+  provisioner "local-exec" {
+    command = <<-EOF
+      @echo off
+      set JAVA_OPTS=-Xmx2048m
+    EOF
+  }
+}
+
+# Privileged execution
+resource "null_resource" "example_privileged_execution" {
+  provisioner "local-exec" {
+    command = <<-EOF
+      runas /user:Administrator "cmd.exe /C echo Hello"
+    EOF
+  }
+}
